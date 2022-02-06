9bf3d267bbbfff556f1dda63a11f76ba46c65d85
Liu Wenyuan <15816141883@163.com>
2022-02-06 09:54:26 +0800

    Update iconsList.ts

--- data/iconsList.ts
+++ data/iconsList.ts
@@ -13,47 +13,20 @@ export const iconsList: IconList[] = [
     icons: [
       "javascript",
       "typescript",
-      "css",
-      "html",
-      "sass",
-      "nodejs",
-      "php",
-      "python",
-      "yaml",
+      "nothing else",
     ],
   },
   {
     namespace: "tools",
-    icons: [
-      "vue",
-      "react",
-      "gulp",
+    icons: [l
       "git",
-      "visual-studio-code",
-      "macos",
-      {
-        name: "vite",
-        url: "https://vitejs.dev/logo.svg",
-      },
+      "of course",
     ],
   },
   {
     namespace: "interested",
     icons: [
-      {
-        name: "blender",
-        url: "https://simpleicons.org/icons/blender.svg",
-      },
-      {
-        name: "cocos",
-        url: "https://user-images.githubusercontent.com/1503156/50446380-ad88c980-094f-11e9-8eff-0094bde708d0.png",
-      },
-      "unity",
-      "unreal-engine",
-      {
-        name: "AE",
-        url: "https://simpleicons.org/icons/adobeaftereffects.svg",
-      },
+      "no",
     ],
   },
 ];
